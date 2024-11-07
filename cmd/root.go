1d1fcece950ac9025584c204ff8e2b0d73b48347
wnykuang <kuangwinnie@gmail.com>
2024-11-06 23:10:36 -0700

    Handle missing path.

--- cmd/root.go
+++ cmd/root.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"flag"
+	"os"
 
 	"github.com/wnykuang/slicegenerator/pkg/video"
 )
@@ -14,12 +15,19 @@ func Execute() {
 	start_time := flag.String("start", "", "Start time of the video")
 	end_time := flag.String("end", "", "End time of the video")
 
+	//optional flag
+	output_path := flag.String("output", "", "Output path of the video")
+
 	flag.Parse()
 
 	// fmt.Println("Video Path: ", *video_path, "Start Time: ", *start_time, "End Time: ", *end_time)
 	// fmt.Println("Arguments: ", args)
 
-	video_clip := video.NewVideoClip(*video_path, *start_time, *end_time)
+	if _, err := os.Stat(*video_path); os.IsNotExist(err) {
+		panic("Video path does not exist")
+	}
+
+	video_clip := video.NewVideoClip(*video_path, *start_time, *end_time, *output_path)
 	video_clip.Print()
 
 }
